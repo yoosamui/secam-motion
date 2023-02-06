efc4d5f945775b464b299908bdec756333171c22
Juan Gonzalez <j-gonzalez@email.de>
2023-02-07 01:41:05 +0700

    integration test for object detector, and recorder. 30 sec video.

--- src/constants.h
+++ src/constants.h
@@ -4,7 +4,7 @@
 #define TMPDIR "/tmp/secam-motion/"
 
 const int c_window_height_offset = 24;
-const int c_videoduration = 10;  // sec.
+const int c_videoduration = 30;  // sec.
 const int c_blur = 6;
 const int c_threshold = 90;
 
