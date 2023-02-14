2f320824005eca053638e7ef4fd7df00b29f60b2
Juan Gonzalez <j-gonzalez@email.de>
2023-02-14 21:25:17 +0700

    remove file by file  after detector finish processing it

--- bin/start-detector.sh
+++ bin/start-detector.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+
 result=`./objdetector -c=0.53 -d=$1 -t=$2`
 
 # if [ "$result" == "1" ]; then
 # do something...
 # fi
-rm -fdr $1
+
+
+
+file=$1
+file=$file$"/image*.jpg"
+rm -f $file
 echo $result
 
 
